99453e9979e16f5d7e9a785ea06d7bc1bb8a7c17
Pandurangan_N <npandu24@gmail.com>
2019-11-17 11:25:52 +0530

    M UI basic setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react'
-import NavBar from './components/NavBar'
+import React, { Component } from 'react';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import NavBar from './components/NavBar';
+import ProTip from './components/ProTip';
+import Copyright from './components/Copyright';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <NavBar />
-      </div>
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
     )
   }
 }
