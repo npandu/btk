d97daa29c4402ee201627fb41e4531a326ee725b
Pandurangan_N <npandu24@gmail.com>
2019-12-02 18:27:34 +0530

    Updates

---
+++ precache-manifest.d175f1ea2c4da864742267f6c553a377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384d698fb4c990e7525197c06c32279f",
+    "url": "/btk/index.html"
+  },
+  {
+    "revision": "447b88bcf19f4b41ce2a",
+    "url": "/btk/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a47f068260fce531d018",
+    "url": "/btk/static/js/2.38c2e49f.chunk.js"
+  },
+  {
+    "revision": "447b88bcf19f4b41ce2a",
+    "url": "/btk/static/js/main.a842fe38.chunk.js"
+  },
+  {
+    "revision": "ed9f6219f23061923b50",
+    "url": "/btk/static/js/runtime-main.33787ee1.js"
+  }
+]);
