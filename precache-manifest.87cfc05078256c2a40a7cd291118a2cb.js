a9d511eb6a48fdc42381369ca95a26cbc0ab2e49
Pandurangan_N <npandu24@gmail.com>
2019-12-06 23:27:15 +0530

    Updates

--- precache-manifest.87cfc05078256c2a40a7cd291118a2cb.js
+++ precache-manifest.87cfc05078256c2a40a7cd291118a2cb.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "952a1df85f233a5833fc2814f0c34eda",
+    "revision": "09e15cd61b2c1e0f9cf84e7310e04a24",
     "url": "/btk/index.html"
   },
   {
-    "revision": "580293a31a85980dce26",
+    "revision": "5c2ac08048d4a05e4a60",
     "url": "/btk/static/css/main.34de6062.chunk.css"
   },
   {
-    "revision": "327c634ec755091088f1",
-    "url": "/btk/static/js/2.32ec7829.chunk.js"
+    "revision": "ed45c3795f2ed7f225a2",
+    "url": "/btk/static/js/2.3c825da7.chunk.js"
   },
   {
-    "revision": "580293a31a85980dce26",
-    "url": "/btk/static/js/main.5c3084d5.chunk.js"
+    "revision": "5c2ac08048d4a05e4a60",
+    "url": "/btk/static/js/main.2697aafd.chunk.js"
   },
   {
     "revision": "ed9f6219f23061923b50",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/btk/precache-manifest.3dfe4697ce085b2e48637003b5105cf2.js"
+  "/btk/precache-manifest.87cfc05078256c2a40a7cd291118a2cb.js"
 );
 
 self.addEventListener('message', (event) => {
